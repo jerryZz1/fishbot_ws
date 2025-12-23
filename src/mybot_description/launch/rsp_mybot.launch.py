465c50351b3377c3fcbee3a6928da34c413e51ca
zmytt0406 <862524030@qq.com>
2025-12-23 23:58:27 +0800

    add mybot_description package

---
+++ src/mybot_description/launch/rsp_mybot.launch.py
@@ -0,0 +1,18 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
+
+def generate_launch_description():
+    pkg = get_package_share_directory('mybot_description')
+    xacro_file = os.path.join(pkg, 'urdf', 'mybot.urdf.xacro')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', xacro_file])}],
+        ),
+    ])
